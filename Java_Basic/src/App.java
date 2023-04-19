631804ff4536a1a32c0eaaee868a325dcdf0427d
rhquddus <rhquddus@naver.com>
2023-04-19 18:32:48 +0900

    commit

--- Java_Basic/src/App.java
+++ Java_Basic/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Car car1 = new Car("람보르기니","검정");
+        car1.setGas(5);
+        car1.setMaxSpeed(320);
+        car1.run();
+        car1.increaseSpeed();
     }
 }
---
+++ Java_Basic/src/Car.java
@@ -0,0 +1,51 @@
+public class Car {
+    private int maxSpeed;
+    private String manufacturer = "현대자동차";
+    private String model;
+    private String color;
+    private int gas;
+    private int step;
+    private int currentSpeed = 10;
+    
+    public Car(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    public void setMaxSpeed(int maxSpeed){
+        this.maxSpeed = maxSpeed;
+    }
+    public void setGas(int gas){
+        this.gas = gas;
+    }
+    public int getCurrentSpeed(){
+        return currentSpeed;
+    }
+    public void setCurrentSpeed(){
+        this.currentSpeed = currentSpeed + 10;
+    }
+    public void resetSpeed(){
+        this.currentSpeed = 0;
+
+    }
+
+    public void run(){
+        while(true){
+            if (gas >0){
+                System.out.println("가스가"+gas+"남아있습니다.");
+                setCurrentSpeed();
+                increaseSpeed();
+                gas -= 1;
+            }
+            else{
+                System.out.println("가스가 없습니다.");
+                resetSpeed();
+                return;
+            }
+        }
+    }
+    public void increaseSpeed(){
+            System.out.println("현재 속도: " + getCurrentSpeed()+ "km");
+            
+    }
+}
